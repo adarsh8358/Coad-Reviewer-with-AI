65e9b93f3c48da7e82f6fde8dfc86acd7a727504
adarsh8358 <adarshkushwaha8358@gmail.com>
2025-12-05 22:13:14 +0530

    test 1

--- Frontend/src/views/home/Home.jsx
+++ Frontend/src/views/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   }
 
   useEffect(() => {
-    axios.get('http://localhost:3000/projects/get-all')
+    axios.get("https://coad-reviewer-with-ai-backend.onrender.com/projects/get-all")
       .then(response => {
         setProjects(response.data.data);
       })
